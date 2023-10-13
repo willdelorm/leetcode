b15fbe9d74abfcdc42e67192d34f0ee767f39c93
Will Delorm <willdelorm@gmail.com>
2023-10-13 15:55:22 -0700

    update time score

--- palindrome_number.js
+++ palindrome_number.js
@@ -3,7 +3,7 @@
 // Runtime: 46 ms, faster than 97.37% of JavaScript online submissions for Two Sum.
 // Memory Usage: 42.5 MB, less than 58.01% of JavaScript online submissions for Two Sum.
 // O(n) time, O(n) space
-// Time to write code: 37 sec
+// Time to write code: 10 sec
 /**
  * @param {number} x
  * @return {boolean}
